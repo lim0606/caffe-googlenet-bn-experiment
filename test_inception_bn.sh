58fd66d0f7bc56d82ff17a3515016b2a69a76e29
Jaehyun Lim <lim0606@gmail.com>
2015-07-08 13:45:53 +0900

    minor modificatoins

--- test_inception_bn.sh
+++ test_inception_bn.sh
@@ -2,4 +2,4 @@
 
 CAFFE_ROOT=/home/jaehyun/github/caffe-dev
 
-$CAFFE_ROOT/build/tools/test_bn -train_model train_val_inception_bn.prototxt -test_model train_val_inception_bn.prototxt -weights snapshots/inception_bn_quick_iter_520000.caffemodel -train_iterations 1000 -gpu 0
+$CAFFE_ROOT/build/tools/test_bn -train_model train_val_inception_bn.prototxt -test_model train_val_inception_bn.prototxt -weights snapshots/inception_bn_quick_iter_600000.caffemodel -train_iterations 1000 -gpu 0
